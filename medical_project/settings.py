c4579efedc3de81ed37f47a77d98ad586c7c2f97
hadi alghoul <hadialghoul2003@gmail.com>
2025-09-01 15:26:20 +0300

    Set DEBUG=False for production

--- medical_project/settings.py
+++ medical_project/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:pdQGsuXfYryFyBFvJrBLTxhxdHNAyeIv@interchange.proxy.rlwy.net:17101/railway")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://postgres:hxFjJYmSCfIgpNVxviVoQzYRXNHWXIaR@yamabiko.proxy.rlwy.net:59699/railway")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
