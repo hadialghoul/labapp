1bd832cc8f987edfacd3d31c2a577a89e87e30e8
hadi alghoul <hadialghoul2003@gmail.com>
2025-09-14 20:57:00 +0300

    pdf online11

--- accounts/supabase_utils.py
+++ accounts/supabase_utils.py
@@ -10,7 +10,7 @@ supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 def upload_pdf_to_supabase(file_path, file_name, bucket_name=SUPABASE_BUCKET):
     # Upload the file
     with open(file_path, 'rb') as f:
-        res = supabase.storage.from_(bucket_name).upload(file_name, f, upsert=True)
+        res = supabase.storage.from_(bucket_name).upload(file_name, f)
     if res.get('error'):
         raise Exception(f"Supabase upload error: {res['error']['message']}")
     # Get the public URL
