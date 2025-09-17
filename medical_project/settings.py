999aa65811ef44d9c44973774eb561e8b666a791
hadi alghoul <hadialghoul2003@gmail.com>
2025-09-17 11:57:06 +0300

    fixed pdf with render

--- medical_project/settings.py
+++ medical_project/settings.py
@@ -200,7 +200,7 @@ WHITENOISE_USE_FINDERS = True
 WHITENOISE_AUTOREFRESH = True
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = '/var/media'
 
 
 # --- ImgBB for images (default storage) ---
